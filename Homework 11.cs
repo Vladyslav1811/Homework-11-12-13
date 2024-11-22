83dae083b4cf624f12c0bbeac58a029ce5dd7c09
Vladyslav1811 <vladkiriluk16@gmail.com>
2024-11-22 18:58:05 +0100

    Add files via upload

---
+++ Homework 11.cs
@@ -0,0 +1,48 @@
+﻿class Hangman
+{
+    static void Main()
+    {
+        string secretWord = "exit";
+        char[] guessedWord = new string('_', secretWord.Length).ToCharArray();
+        int attemptsLeft = 6;
+
+        Console.WriteLine("Welcome! Try to guess the hidden word!");
+        Console.WriteLine($"Number of letters in the word: {secretWord.Length}");
+        Console.WriteLine($"Number of allowed wrong guesses: {attemptsLeft}");
+
+        while (attemptsLeft > 0 && new string(guessedWord).Contains("_"))
+        {
+            Console.Write("\nEnter your letter: ");
+            char guess = Console.ReadLine().ToLower()[0];
+
+            if (secretWord.Contains(guess))
+            {
+                Console.Write($"This letter is in the word! Letter position(s): ");
+                for (int i = 0; i < secretWord.Length; i++)
+                {
+                    if (secretWord[i] == guess)
+                    {
+                        guessedWord[i] = guess;
+                        Console.Write($"{i + 1} ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                attemptsLeft--;
+                Console.WriteLine($"This letter is not in the word! Attempts left: {attemptsLeft}");
+            }
+        }
+        if (!new string(guessedWord).Contains("_"))
+        {
+            Console.WriteLine($"\nCongratulations, you guessed the word! The hidden word was: {secretWord}.");
+        }
+        else
+        {
+            Console.WriteLine($"\nSorry, you lost. The hidden word was: {secretWord}.");
+        }
+
+        Console.WriteLine("Thanks for playing.");
+    }
+}
---
+++ Homework 12.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    static int GetFibonacci(int n)
+    {
+        if (n < 0)
+            throw new ArgumentException("Input must be a non-negative integer.");
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the Fibonacci sequence index (non-negative integer):");
+        if (int.TryParse(Console.ReadLine(), out int index) && index >= 0)
+        {
+            try
+            {
+                int result = GetFibonacci(index);
+                Console.WriteLine($"The Fibonacci number at index {index} is: {result}");
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a non-negative integer.");
+        }
+    }
+}
